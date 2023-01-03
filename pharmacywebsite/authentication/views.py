50247c3475f38e187524b470689e99ecc9a9c84f
sixdgt <c4crypt@gmail.com>
2023-01-03 08:59:00 +0545

    user register views

--- pharmacywebsite/authentication/views.py
+++ pharmacywebsite/authentication/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.views import View
+from django.contrib.auth.models import User
+# from django.contrib import auth
 
 # Create your views here.
 class LoginView(View):
@@ -8,4 +10,14 @@ class LoginView(View):
 
 class RegisterView(View):
     def get(self, request):
+        # return HttpResponse("Hello World")
+        return render(request, 'authentication/register.html')
+    
+    def post(self, request):
+        req_email = request.POST.get('email'),
+        req_password = request.POST.get('password'),
+        req_username = request.POST.get('username'),
+        user = User.objects.create_user(email=req_email, username=req_username)
+        user.set_password(req_password)
+        user.save()
         return render(request, 'authentication/register.html')
