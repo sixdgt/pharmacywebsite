5353f35611abe0d83875e7d363b26a30887d4742
sixdgt <c4crypt@gmail.com>
2023-01-08 09:01:23 +0545

    medicine model and list template created

--- pharmacywebsite/medicines/models.py
+++ pharmacywebsite/medicines/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from datetime import datetime
 # Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = "app_categories"
+
+class Medicine(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    expiry_date = models.DateTimeField()
+    mg = models.IntegerField()
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    entry_date = models.DateTimeField(default=datetime.now)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "app_medicines"
